4f82ee62d05aed786e70fed80df546ac3b9b73c5
Marcel Balint <blank_blankness@yahoo.com>
2019-11-30 02:03:37 -0500

    Created an asynchronous function to fetch the data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const Api_Key = "79b63a8e14c9e0cb2d5cfaf0d76b208a";
+
 class App extends Component {
+  getWeatherData = async () => {
+    const getData = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=London&appid=${Api_Key}`
+    );
+    const parsedData = await getData.json();
+    console.log(parsedData);
+  };
+
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <button onClick={this.getWeatherData}>Show</button>
+      </div>
+    );
   }
 }
 
