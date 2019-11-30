1021386e12c75c97ea047dd49ac5ee05cd684589
Marcel Balint <blank_blankness@yahoo.com>
2019-11-30 02:10:06 -0500

    Created an 'Input' component

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Input = () => (
+  <div>
+    <form>
+      <input type="text" name="text" placeholde="Type a city.."></input>
+      <button>Search</button>
+    </form>
+  </div>
+);
+
+export default Input;
