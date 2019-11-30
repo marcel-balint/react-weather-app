2b7c64ac743144aa985aa7de5723701f1d5f63ed
Marcel Balint <blank_blankness@yahoo.com>
2019-11-30 03:12:50 -0500

    Passed the state properies to 'Weather' component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 
 import Input from "./components/Input/Input";
+import Weather from "./components/Weather/Weather";
 
 const Api_Key = "79b63a8e14c9e0cb2d5cfaf0d76b208a";
 
@@ -37,6 +38,14 @@ class App extends Component {
     return (
       <div className="App">
         <Input showWeather={this.getWeatherData} />
+        <Weather
+          city={this.state.city}
+          country={this.state.country}
+          mainTemperature={this.state.mainTemperature}
+          temp_max={this.state.temp_max}
+          temp_min={this.state.temp_min}
+          description={this.state.description}
+        />
       </div>
     );
   }
--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Weather = () => {
-  return <div></div>;
+const Weather = props => {
+  return (
+    <div>
+      <h3>
+        Weather in {props.city}, {props.country}:
+      </h3>
+      <div>
+        <p> {props.description}</p>
+        <p>
+          <strong>{props.mainTemperature}&deg;C</strong>
+        </p>
+        <p>
+          Max: <strong>{props.temp_max}&deg;C</strong>
+        </p>
+        <p>
+          Min: <strong>{props.temp_min}&deg;C</strong>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Weather;
