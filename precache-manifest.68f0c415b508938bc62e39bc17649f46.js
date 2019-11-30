952fd8ab6adafa74736e88679677154b58045302
Marcel Balint <blank_blankness@yahoo.com>
2019-11-30 06:47:56 -0500

    Updates

---
+++ precache-manifest.68f0c415b508938bc62e39bc17649f46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6e4c52d3938e5e3e5bc31890230d8a",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "5b0db768e7fd4f654f95",
+    "url": "/react-weather-app/static/css/main.77c2a020.chunk.css"
+  },
+  {
+    "revision": "f372b03d0c49f6ee5feb",
+    "url": "/react-weather-app/static/js/2.62d16c63.chunk.js"
+  },
+  {
+    "revision": "5b0db768e7fd4f654f95",
+    "url": "/react-weather-app/static/js/main.9bf493ed.chunk.js"
+  },
+  {
+    "revision": "d3974777b71e8982ab4e",
+    "url": "/react-weather-app/static/js/runtime-main.2e70847b.js"
+  }
+]);
